b5cda71c76ded3b771361ed7bee0edc49184cbb2
xpilasi <pilasiha@gmail.com>
2025-07-18 23:24:55 +0200

    about paddings

--- src/views/AboutView/AboutView.vue
+++ src/views/AboutView/AboutView.vue
@@ -5,7 +5,7 @@ export default {
 </script>
 
 <template>
-  <div class="bg-bg-primary min-h-screen px-4  xl:px-48">
+  <div class="bg-bg-primary min-h-screen px-4  xl:px-48 2xl:px-80">
     <h1>AboutView</h1>
   </div>
 </template>
